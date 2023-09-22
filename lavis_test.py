f05c7900edba9616ecb0aab755bd5e62659b3acc
Jarrod Sears <Jarrod@mangatanetworks.com>
2023-09-22 15:43:16 -0800

    basic working example of LAVIS blip diffusion

--- lavis_test.py
+++ lavis_test.py
@@ -3,8 +3,66 @@ import torch
 from PIL import Image
 from lavis.models import load_model_and_preprocess
 
-print("Is cuda available: " + torch.cuda.is_available())
+print("Is cuda available: " + str(torch.cuda.is_available()))
 
 
 model, vis_preprocess, txt_preprocess = load_model_and_preprocess("blip_diffusion", "base", device="cuda", is_eval=True)
 
+cond_subject = "dog"
+src_subject = "cat"
+tgt_subject = "dog"
+
+text_prompt = "sit on sofa"
+
+src_subject = txt_preprocess["eval"](src_subject)
+tgt_subject = txt_preprocess["eval"](tgt_subject)
+cond_subject = txt_preprocess["eval"](cond_subject)
+text_prompt = [txt_preprocess["eval"](text_prompt)]
+
+cond_image = Image.open("../images/dog.png").convert("RGB")
+cond_image.resize((256, 256)).save("resized_dog.png");
+cond_image = vis_preprocess["eval"](cond_image).unsqueeze(0).cuda()
+
+src_image = Image.open("../images/cat-sofa.png").convert("RGB")
+
+samples = {
+    "cond_images": cond_image,
+    "cond_subject": cond_subject,
+    "src_subject": src_subject,
+    "tgt_subject": tgt_subject,
+    "prompt": text_prompt,
+    "raw_image": src_image,
+}
+
+iter_seed = 88871
+guidance_scale = 7.5
+num_inference_steps = 50
+num_inversion_steps = 50 # increase to improve DDIM inversion quality
+negative_prompt = "over-exposure, under-exposure, saturated, duplicate, out of frame, lowres, cropped, worst quality, low quality, jpeg artifacts, morbid, mutilated, out of frame, ugly, bad anatomy, bad proportions, deformed, blurry, duplicate"
+
+output = model.edit(
+    samples,
+    seed=iter_seed,
+    guidance_scale=guidance_scale,
+    num_inference_steps=num_inference_steps,
+    num_inversion_steps=num_inversion_steps,
+    neg_prompt=negative_prompt,
+)
+
+print("=" * 30)
+print("Before editing:")
+output[0].save("before_editing.png")
+
+print("After editing:")
+output[1].save("after_editing.png")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# loads BLIP caption base model, with finetuned checkpoints on MSCOCO captioning dataset.
+# this also loads the associated image processors
+model, vis_processors, _ = load_model_and_preprocess(name="blip_caption", model_type="base_coco", is_eval=True, device=device)
+# preprocess the image
+# vis_processors stores image transforms for "train" and "eval" (validation / testing / inference)
+image = vis_processors["eval"](src_image).unsqueeze(0).to(device)
+# generate caption
+caption_img = model.generate({"image": image})
+caption_img.save("caption_img.png")
